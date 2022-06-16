7389b5e3dd94a47a00c13461ca7450d5738629a0
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-06-16 23:06:39 +0900

    チャンネル内でリアクションをした投稿一覧を取得することに成功

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,10 @@
-// import 'dotenv/config'
-// import { App } from '@slack/bolt'
-
-// const app = new App({
-//   token: process.env.BOT_USER_OAUTH_TOKEN,
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-//   socketMode: true,
-//   appToken: process.env.SLACK_APP_TOKEN,
-//   port: 3000
-// })
-
-// app.message(':wave:', async({ message, say }) => {
-//   console.log(message)
-// })
-	
-// ;(async() => {
-//   await app.start()
-//   console.log('Bolt app is Running!')
-// })()
-
 import express from 'express'
+import axios from 'axios'
+import 'dotenv/config'
+
 const app: express.Express = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-//CROS対応（というか完全無防備：本番環境ではだめ絶対）
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', '*')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})
 
 app.listen(3000, () => {
   console.log('Start on port 3000.')
@@ -37,8 +12,23 @@ app.listen(3000, () => {
 
 app.post('/', (req, res) => {
   console.log(req.body)
+  console.log(req)
   res.header('Content-type', 'application/json')
   res.set('Content-type', 'application/json')
 
   res.status(200).json({ challenge: req.body.challenge })
+})
+
+app.get('/slack/reactions', async(req, res) => {
+  const endPoint = `https://slack.com/api/reactions.list?user=${process.env.USER_ID}&pretty=1`
+  console.log(process.env.USER_ID)
+  const reactions = await axios.get(endPoint, {
+    headers: {
+      Authorization: `Bearer ${process.env.BOT_USER_OAUTH_TOKEN}`,
+    }
+  }).then((res) => {
+    console.log(res.data)
+  })
+
+  return reactions
 })
