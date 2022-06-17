803c4c8bb54109138e7bda01ee95b718a7f02b62
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-06-17 19:34:27 +0900

    SlackAPIのレスポンス値の型定義

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,72 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+interface SlackReactionData {
+  ok: boolean
+  items: {
+    type: string
+    channel: string
+    message: {
+      type: string
+      subtype: string
+      text: string
+      ts: string
+      username: string
+      icons: {
+        image_36: string
+        image_48: string
+        image_72: string
+      }
+      bot_id: string
+      attachments: {
+        from_url: string
+        ts: number
+        id: number
+        original_url: string
+        fallback: string
+        text: string
+        pretext: string
+        title: string
+        title_link: string
+        author_name: string
+        author_link: string
+        author_icon: string
+        author_subname: string
+        service_name: string
+        service_url: string
+        mrkdwn_in: string[],
+        footer: string[],
+        footer_icon: string
+      }[]
+      reactions: {
+        name: string
+        users: string[]
+        count: number
+      }[]
+      permalink: string
+    }
+  }[]
+  response_metadata: {
+    next_cursor: string
+  }
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`https://slack.com/api/reactions.list?user=${process.env.USER_ID}&pretty=1`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${process.env.BOT_USER_OAUTH_TOKEN}`,
+    }
+  })
+
+  const reactionList: SlackReactionData = await res.json()
+  console.log(reactionList.items[0].message.attachments)
+  console.log(reactionList.items[0].message.reactions)
+  return {
+    props: {}
+  }
+}
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
