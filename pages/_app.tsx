6c74dcfbc7271554f2312d673e3db53a91fc902f
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-06-19 21:48:47 +0900

    Headコンポーネントの削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Head } from '@components/common'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head />
+      <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
