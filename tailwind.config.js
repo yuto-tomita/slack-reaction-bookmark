1a149293714d13801480a2b31a2aeef634b7e788
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-07-02 22:47:12 +0900

    tailwindcssの導入

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{ts,tsx,jsx,js}',
+    './pages/**/*.{tsx,jsx,js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
